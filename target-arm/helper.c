d9c64840753566259c44a1ca0bf4aae53e27f0a0
Fabian Aggeler <aggelerf@ethz.ch>
2014-06-11 01:55:04 +0200

    target-arm: make CSSELR banked
    Rename CSSELR (cache size selection register) and add secure
    instance (AArch32).
    
    Signed-off-by: Fabian Aggeler <aggelerf@ethz.ch>
    Signed-off-by: Greg Bellows <greg.bellows@linaro.org>
    
    ==========
    
    v5 -> v6
    - Changed _el field variants to be array based
    - Switch to use distinct CPREG secure flags.
    - Merged CSSELR and CSSELR_EL1 reginfo entries
    
    v4 -> v5
    - Changed to use the CCSIDR cpreg bank flag to select the csselr bank instead
      of the  A32_BANKED macro.  This more accurately uses the secure state bank
      matching the CCSIDR.

--- target-arm/helper.c
+++ target-arm/helper.c
@@ -789,7 +789,14 @@ static void scr_write(CPUARMState *env, const ARMCPRegInfo *ri, uint64_t value)
 static uint64_t ccsidr_read(CPUARMState *env, const ARMCPRegInfo *ri)
 {
     ARMCPU *cpu = arm_env_get_cpu(env);
-    return cpu->ccsidr[env->cp15.c0_cssel];
+
+    /* Acquire the CSSELR index from the bank corresponding to the CCSIDR
+     * bank
+     */
+    uint32_t index = A32_BANKED_REG_GET(env, csselr,
+                                 ARM_CP_SECSTATE_TEST(ri, ARM_CP_SECSTATE_S));
+
+    return cpu->ccsidr[index];
 }
 
 static void csselr_write(CPUARMState *env, const ARMCPRegInfo *ri,
@@ -915,9 +922,10 @@ static const ARMCPRegInfo v7_cp_reginfo[] = {
       .opc0 = 3, .crn = 0, .crm = 0, .opc1 = 1, .opc2 = 0,
       .access = PL1_R, .readfn = ccsidr_read, .type = ARM_CP_NO_MIGRATE },
     { .name = "CSSELR", .state = ARM_CP_STATE_BOTH,
-      .opc0 = 3, .crn = 0, .crm = 0, .opc1 = 2, .opc2 = 0,
-      .access = PL1_RW, .fieldoffset = offsetof(CPUARMState, cp15.c0_cssel),
-      .writefn = csselr_write, .resetvalue = 0 },
+      .cp = 15, .crn = 0, .crm = 0, .opc1 = 2, .opc2 = 0,
+      .access = PL1_RW, .writefn = csselr_write, .resetvalue = 0,
+      .bank_fieldoffsets = { offsetof(CPUARMState, cp15.csselr_s),
+                             offsetof(CPUARMState, cp15.csselr_ns) } },
     /* Auxiliary ID register: this actually has an IMPDEF value but for now
      * just RAZ for all cores:
      */
